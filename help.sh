39934a43da8972ab3ba7c7f8ea4ad7e530871dd4
Thino-kun <thiago.firmino@leaderempresarial.com.br>
2026-02-25 10:14:12 -0300

    feat: update help string

--- help.sh
+++ help.sh
@@ -7,15 +7,16 @@ USE:
 	Make sure to have a '.env' file with required and/or optional variables to make it works
 
 VARIABLES:
-	USERNAME: Username of your remote repository account
-	HOST: host of your hub (for default is 'git@github.com:')
-	REPO_URL: host layout, like https://$(HOST)/$(USERNAME)/$(REPO)
+	USERNAME: Username of your remote repository account (REQUIRED)
+	HOST: host of your hub (for default is 'git@github.com:') (OPTIONAL)
+	REPO_URL: host layout, like https://$(HOST)/$(USERNAME)/$(REPO) $(OPTIONAL)
 
 RULES:
 	Rules are project tests.
 
 	help - display moulinette help
 	eu-aceito - tests 'Eu Aceito' projects
+	shell-00 - tests 'Shell 00' projects
 	trace - display all project trace logs
 EOF
 )"
